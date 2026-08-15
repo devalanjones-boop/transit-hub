bd1a9087bf2e932aadfa67ff49ada85914350162
Alan Jones <dev.alanjones@gmail.coom>
2026-08-15 20:09:23 +0530

    started schedule

--- server/validations/scheduleValidation.js
+++ server/validations/scheduleValidation.js
@@ -21,5 +21,52 @@ const objectIdValidator = (value, helper) => {
 }
 
 const scheduleValidattioSchema = Joi.object({
-    
+    busId: Joi.string().custom(objectIdValidator).required().messages({
+        "any.invalid": "busId must be a valid MongoDB ObjectId",
+        "any.required":"busId is required",
+    }),
+    routeId: Joi.string().custom(objectIdValidator).required().messages({
+        "any.invalid": "routeId must be a valid MongoDB ObjectId",
+        "any.required":"routeId is required",
+    }),
+    stopId: Joi.string().custom(objectIdValidator).required().messages({
+        "any.invalid": "stopId must be a valid MongoDB ObjectId",
+        "any.required":"stopId is required",
+    }),
+    departureTime: Joi.string().pattern(timeRegex).required().messages({
+        "string.pattern.base": "departureTime must be in HH:mm 24-hour format",
+        "any.required": "departureTime is required"
+    }),
+    arrivalTime: Joi.string().pattern(timeRegex).required().messages({
+        "string.pattern.base": "arrivalTime must be in HH:mm 24-hour format",
+        "any.required": "arrivalTime is required"
+    }),
+    days: Joi.array()
+    .items(Joi.string().valid(...daysEnum))
+    .min(1)
+    .required()
+    .messages({
+        "array.min":"At least one operational day must be specified",
+        "any.required": "days are required",
+    }),
+    status: Joi.string()
+    .valid("ON_TIME", "DELAYED", "CANCELLED", "COMPLETED")
+    .default("ON_TIME")
 })
+
+const updateScheduleValidationSchema = Joi.object({
+  busId: Joi.string().custom(objectIdValidator),
+  routeId: Joi.string().custom(objectIdValidator),
+  stopId: Joi.string().custom(objectIdValidator),
+  departureTime: Joi.string().pattern(timeRegex),
+  arrivalTime: Joi.string().pattern(timeRegex),
+  days: Joi.array()
+    .items(Joi.string().valid(...daysEnum))
+    .min(1),
+  status: Joi.string().valid("ON_TIME", "DELAYED", "CANCELLED", "COMPLETED"),
+}).min(1);
+
+module.exports = {
+    scheduleValidattioSchema,
+    updateScheduleValidationSchema
+}
